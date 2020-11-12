642f5faf5ad37c2055a2102daedfd90538e4bfa0
shoumen <0showman0@gmail.com>
2020-11-12 21:38:14 +0400

    Prepared

--- sockets.js
+++ sockets.js
@@ -1,31 +1,13 @@
-const fs = require('fs');
-
 module.exports = function () {
-    const server = require('http').createServer(function (req, res) {
-        console.log(req.url);
-
-        if (req.url === "/favicon.ico") {
-            res.writeHead(404);
-            res.end();
-            return;
-        }
-
-        res.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
-        fs.createReadStream('index.html').pipe(res);
-    });
-
-    server.listen(80);
-
-// WebSockets
-    const wsServer = require('http').createServer();
-    const io = require('socket.io')(wsServer, {
+    const server = require('http').createServer();
+    const io = require('socket.io')(server, {
         cors: {
             origin: '*',
             credentials: true
         },
     });
 
-    wsServer.listen(3000);
+    server.listen(3000);
 
     return io;
 }
